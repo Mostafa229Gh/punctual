9f63c6bdb7f14c765eb40dcadf345e312b770e04
Mostafa229Gh <Mostafa229Gh@gmail.com>
2025-06-11 16:19:57 +0330

    INIT

---
+++ views/auth/login.php
@@ -0,0 +1,14 @@
+<div class="Login_form">
+    <div class="circle"></div>
+    <div class="circle_2"></div>
+    <form action="/login" method="post" class="form_section" autocomplete="off">
+        <h2 class="form_title">Login Here</h2>
+        <label for="username" class="lable_form">Username</label>
+        <input type="text" id="username" class="input_username" name="username" placeholder="Email or Phone" required>
+        <br>
+        <label for="password" class="lable_form">Password</label>
+        <input type="password" id="password" class="input_Password" name="password" placeholder="Password" required>
+        <br>
+        <button class="button_submit" type="submit">Log In</button>
+    </form>
+</div>
